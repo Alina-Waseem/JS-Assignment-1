78a2dad50461ec02234fd5d44e5a86100561160c
Alina <139748396+Alina-Waseem@users.noreply.github.com>
2023-10-06 01:49:36 +0500

    Add files via upload

---
+++ Ex-1.4.js
@@ -0,0 +1,10 @@
+// Set a variable 'a' to the value 10.
+let a = 10;
+
+// Log the value of 'a' to the console. This will display the value of 'a' in the browser's console.
+console.log("The value of 'a' is:", a);
+
+/*
+   This is a multiple-line comment.
+   In a production script, you might use this space to add a brief outline of the purpose of the file.
+*/
